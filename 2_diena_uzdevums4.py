f21086f803a0e1f0a8ed47d2d5bd1d39325077dd
Inga <ingabertule@inbox.lv>
2022-03-10 20:13:41 +0200

    Pievienots 4. uzdevums

---
+++ 2_diena_uzdevums4.py
@@ -0,0 +1,20 @@
+# 4. uzdevums
+# Lietotājs ievada savu vērtējumu programmēšanā izvadīt vārdisko vērtējumu (izcili, teicami utt.)
+
+skaitlis = int(input("Ievadi skaitli:"))
+if skaitlis <= 3:
+    print("Negatīvs vērtējums")
+if skaitlis == 4:
+    print("Gandrīz viduvēji")
+if skaitlis == 5:
+    print("Viduvēji")
+if skaitlis == 6:
+    print("Gandrīz labi")
+if skaitlis == 7:
+    print("Labi")
+if skaitlis == 8:
+    print("Ļoti labi")
+if skaitlis == 9:
+    print("Teicami")
+if skaitlis == 10:
+    print("Izcili")
