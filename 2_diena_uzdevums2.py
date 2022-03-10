230bdc80bbb4d3fa4d1459092ce865844f64b0b4
Inga <ingabertule@inbox.lv>
2022-03-10 19:43:48 +0200

    Pievienots 2. uzdevums

--- 2_diena_uzdevums2.py
+++ 2_diena_uzdevums2.py
@@ -1,4 +1,6 @@
 # 2. uzdevums
+#Uzrakstiet programmu, kas prasa lietotājam ievadīt savu vārdu, ja tas ievada vārdu, Bond, tad izdrukājiet uz ekrāna "Esi sveicināts 007",
+#ja cits tad "Esi sveicināts, VARDS", kur VARDS ir lietotāja ievadītais vārds.
 
 vards = (input("Kā Jūs sauc?"))
 
