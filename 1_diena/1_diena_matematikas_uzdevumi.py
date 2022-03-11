18ccc932a2ad311702554b2a3ed3a4a89742f5eb
Inga <ingabertule@inbox.lv>
2022-03-11 11:28:18 +0200

    Pievienoti matemātikas uzdevumu risinājumi

---
+++ 1_diena/1_diena_matematikas_uzdevumi.py
@@ -0,0 +1,22 @@
+# 1. uzdevums
+# Dotas divas taisnstūra malas 4, 7 aprēķināt taisnstūra laukumu.
+
+mala_A = 4
+mala_B = 7
+laukums = (mala_A * mala_B)
+print("Taisnstūra laukums ir: ",laukums)
+
+# 2. uzdevums
+# Dota temperatūra Celsija grādos 21, cik tas būs Fārenheiti?
+
+celsius = 27
+fahrenheit = (celsius * 9/5) + 32
+print("Fārenheiti:", fahrenheit)
+
+# 3. uzdevums
+# Dots riņķa līnijas diametrs 7, aprēķināt riņķa līnijas garumu.
+
+import math
+garums = round (7 *math.pi, 2)
+
+print("Riņķa līnijas garums = ", garums)
